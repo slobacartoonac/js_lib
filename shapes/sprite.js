543b5874e028ddbbfc7bdf51f952f4cb08a885b2
Slobodan <slobacartoonac@gmail.com>
2022-08-16 09:46:18 +0200

    Remove ugly animation

--- shapes/sprite.js
+++ shapes/sprite.js
@@ -1,14 +1,5 @@
-import { AnimationSprite } from "../actors/animated_sprite"
-
 function Sprite(image) {
 	this.image = image
-	this.animation = null
-	this.setAnimation = (images) => {
-		this.animation = new AnimationSprite(images, this)
-	}
-	this.tick = () => {
-		this.animation && this.animation.tick()
-	}
 }
 
 export { Sprite }
