bd4fbd6bf0ea666c085159b0ec23a6d9c51750ff
Slobodan <slobacartoonac@gmail.com>
2022-09-19 15:13:41 +0200

    Add gray background on fps counter

--- ecs/drawers/drawFPS.js
+++ ecs/drawers/drawFPS.js
@@ -1,20 +1,25 @@
-function FPSPloter(context){
+function FPSPloter(context) {
 	this.context = context
 	this.time = (new Date()).getTime()
-	this.i=0
-	this.fps=0
+	this.i = 0
+	this.fps = 0
 }
 
-FPSPloter.prototype.draw=function()
-{
+FPSPloter.prototype.draw = function () {
 	this.i++
 	var newTime = (new Date()).getTime()
 	var deltaT = newTime - this.time
 	this.time = newTime
 	this.context.font = '14px Verdana'
+
+	if (!(this.i % 30)) this.fps = (Math.round(10000 / deltaT) / 10) + ' fps'
+	this.context.beginPath();
+	this.context.rect(10, 13, 60, 14);
+	this.context.fillStyle = 'gray'
+	this.context.fill();
 	this.context.fillStyle = 'red'
-	if(!(this.i%30)) this.fps =(Math.round(10000/deltaT)/10) + ' fps'
 	this.context.fillText(this.fps, 10, 24)
+
 }
 
 export default FPSPloter
