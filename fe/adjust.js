9330e432ac77d95399d5eaf4aa6964a045d938fe
Slobodan <slobacartoonac@gmail.com>
2022-10-12 15:26:50 +0200

    Add adjust type

--- fe/adjust.js
+++ fe/adjust.js
@@ -1,7 +1,8 @@
-function Adjust() {
+function Adjust(type) {
     this.subs = {};
     this.last_id = 0;
     this.last_value = null;
+    this.type = type
 
     this.subscribe = (func) => {
         if (this.last_value) {
@@ -17,6 +18,9 @@ function Adjust() {
     }
 
     this.publish = (arg) => {
+        if (this.type && this.type.name !== arg.constructor.name) {
+            throw Error(`arg ${arg} type ${arg.constructor.name} is not type of ${this.type.name}`)
+        }
         this.last_value = arg;
         Object.values(this.subs).forEach((func) => {
             func(arg);
