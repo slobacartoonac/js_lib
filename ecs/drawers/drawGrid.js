feeae8b3c6c0df01f68c85b816cead425b1ccb99
Slobodan <slobacartoonac@gmail.com>
2023-01-02 15:00:22 +0100

    Add axis cords, options

--- ecs/drawers/drawGrid.js
+++ ecs/drawers/drawGrid.js
@@ -1,7 +1,12 @@
+import { screenToWorld } from "../../math/view"
+import { interpolate } from "../../math/vec"
 
-function GridPloter(ctx, showCords) {
+function GridPloter(ctx, options) {
 	this.context = ctx
-	this.showCords
+	this.showCords = options?.showCords
+	this.axisCords = options?.axisCords
+	this.axisScale = options?.axisScale || 100
+	this.devide = options?.devide || 2
 }
 
 GridPloter.prototype.draw = function (sizex, sizey, view) {
@@ -24,36 +29,100 @@ GridPloter.prototype.draw = function (sizex, sizey, view) {
 	for (var x = startx; x <= canvasWidth; x += stepX) {
 		context.moveTo(x, 0)
 		context.lineTo(x, canvasHeight)
+		if (!this.axisCords) continue;
+		let cordX = screenToWorld(
+			centerX,
+			centerY,
+			scale,
+			canvasWidth,
+			canvasHeight,
+			x,
+			0
+		)[0]
+		this.drawCod(cordX, 0, view, context)
 	}
 
 	for (var y = starty; y <= canvasHeight; y += stepY) {
 		context.moveTo(0, y)
 		context.lineTo(canvasWidth, y)
+		if (!this.axisCords) continue;
+		let cordY = screenToWorld(
+			centerX,
+			centerY,
+			scale,
+			canvasWidth,
+			canvasHeight,
+			0,
+			y
+		)[1]
+		this.drawCod(0, cordY, view, context)
 	}
 	context.lineWidth = 2
 	context.strokeStyle = 'rgba(128, 128, 128, 0.5)'
 	context.stroke()
+	for (var i = 0; i < this.devide - 1; i++) {
+		var startx = (startx + stepX / this.devide) % stepX
+		var starty = (starty + stepY / this.devide) % stepY
 
-	var startx = (startx + stepX / 2) % stepX
-	var starty = (starty + stepY / 2) % stepY
+		context.beginPath()
+		for (var x = startx; x <= canvasWidth; x += stepX) {
+			context.moveTo(x, 0)
+			context.lineTo(x, canvasHeight)
+		}
 
-	context.beginPath()
-	for (var x = startx; x <= canvasWidth; x += stepX) {
-		context.moveTo(x, 0)
-		context.lineTo(x, canvasHeight)
+		for (var y = starty; y <= canvasHeight; y += stepY) {
+			context.moveTo(0, y)
+			context.lineTo(canvasWidth, y)
+		}
+		context.lineWidth = 1
+		context.strokeStyle = 'rgba(128, 128, 128, ' + interpolate(0.25, 0.75, gridScale - 1) + ')'
+		context.stroke()
 	}
-
-	for (var y = starty; y <= canvasHeight; y += stepY) {
-		context.moveTo(0, y)
-		context.lineTo(canvasWidth, y)
-	}
-	context.lineWidth = 2
-	context.strokeStyle = 'rgba(128, 128, 128, ' + (gridScale - 1.0) + ')'
-	context.stroke()
 	if (this.showCords) {
 		context.font = '10px Arial'
-		context.fillText((Math.round(view.x * 100) / 100 + Number.EPSILON) + ',' + (Math.round(view.y * 100 + Number.EPSILON) / 100) + ',' + (Math.round(view.scale * 100 + Number.EPSILON) / 100), 10, 50)
+		context.fillText(
+			(Math.round(view.x * 100) / 100 + Number.EPSILON) +
+			',' + (Math.round(view.y * 100 + Number.EPSILON) / 100) +
+			',' + (Math.round(view.scale * 100 + Number.EPSILON) / 100), 10, 50)
 	}
+}
+
+
+GridPloter.prototype.drawCod = function (eX, eY, view, context) {
+	const { x: centerX, y: centerY, scale } = view
+	var canvasWidth = context.canvas.clientWidth
+	var canvasHeight = context.canvas.clientHeight
+	const canvasWidthHalf = canvasWidth / 2
+	const canvasHeightHalf = canvasHeight / 2
+	var x = (eX - centerX) * scale + canvasWidthHalf
+	var y = (eY - centerY) * scale + canvasHeightHalf
+	if (x < 0 || y < 0 || x > canvasWidth || y > canvasHeight)
+		return
+	// const elementSize = 5 * scale > 1 ? 5 * scale : 1
+	// context.beginPath()
+	// context.arc(x, y, elementSize, 0, 2 * Math.PI, false)
+	// context.lineWidth = 1
+	// context.strokeStyle = element[3]
+	// context.stroke()
+	context.font = '14px Verdana'
+	context.fillStyle = 'rgb(0, 0, 0, 1.0)'
+	function trimNum(value) {
+		return +parseFloat(value).toFixed(2)
+	}
+
+	let text = ""
+	let ext = trimNum((eX / this.axisScale))
+	let eyt = trimNum((-eY / this.axisScale))
+	if (ext) {
+		text += ext
+	}
+	if (eyt) {
+		text += eyt
+	}
+	if (!eyt && !ext) {
+		text += "0"
+	}
+	context.fillText(text, x + 5, y + 14)
 }
 
 GridPloter.prototype.line = function (startx, starty, sizex, sizey, color) {
