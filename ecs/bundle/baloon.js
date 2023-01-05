2dc602bec21b27f8a6cd8c76af91aaeb28ed7c4a
Slobodan <slobacartoonac@gmail.com>
2023-01-05 22:19:34 +0100

    Functional baloon

--- ecs/bundle/baloon.js
+++ ecs/bundle/baloon.js
@@ -13,12 +13,16 @@ function setAsociation(manager, ...boxes) {
     boxes.forEach(box => manager.asign(new baloonElement(), box))
 }
 
-function makeBaloon(manager, renderer, positions, text, options) {
+export function createTextBaloon(manager, renderer, positions, text, options) {
+    let entities = []
     let stabiley = positions[1]
     let textWidth = 0
     let textChunks = chunkString(text)
     let textSize = options?.textSize || 20
     let lineHeight = options?.lineHeight || textSize + 10
+    let background = options?.background || '#ffffffaa'
+    let borderColor = options?.borderColor || '#ffffff'
+    let borderWidth = options?.borderWidth || 1
     let shapes = textChunks.map((str) => {
         let shapeText = new ShapeText(textSize, str)
         textWidth = Math.max(renderer.mesure(shapeText).width, textWidth)
@@ -26,20 +30,22 @@ function makeBaloon(manager, renderer, positions, text, options) {
     })
     let boxes = createBodrededBoxColor(
         manager,
-        positions[0],
+        positions[0] - textWidth / 2 - 5,
         stabiley,
         textWidth + 10,
         shapes.length * lineHeight + 10,
-        '#ffffffaa',
-        { color: '#ffffff', width: 1 }
+        background,
+        { color: borderColor, width: borderWidth }
     )
+    entities.push(...boxes)
     setAsociation(manager, ...boxes)
     shapes.map((shapeText, line) => {
         let entityText = manager.create()
-        manager.asign(new Transform([positions[1] - textWidth - 20, stabiley + line * lineHeight + 5]), entityText)
+        manager.asign(new Transform([positions[0] - textWidth / 2, stabiley + line * lineHeight + 5]), entityText)
         manager.asign(shapeText, entityText)
         manager.asign(new Renderer('#000000', null, 2), entityText)
+        entities.push(entityText)
         setAsociation(manager, entityText)
     })
-    return stabiley
+    return entities
 }
