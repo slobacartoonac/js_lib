12f2c542cac78d14de064d9e59787645dcf717cc
Slobodan <slobacartoonac@gmail.com>
2023-01-06 13:14:37 +0100

    Adaptable baloon

--- ecs/bundle/baloon.js
+++ ecs/bundle/baloon.js
@@ -2,7 +2,7 @@ import { ShapeText } from "../../shapes/text";
 import { Renderer } from "../drawers/render";
 import { Transform } from "../physics/transform";
 import { createBodrededBoxColor } from "./element";
-import { chunkString } from "./tools";
+import { chunkString, chunkStringLong } from "./tools";
 
 //chunkString
 //chunkStringLong
@@ -15,8 +15,6 @@ function setAsociation(manager, ...boxes) {
 
 export function createTextBaloon(manager, renderer, positions, text, options) {
     let entities = []
-    let textWidth = 0
-    let textChunks = chunkString(text)
     let textSize = options?.textSize || 20
     let lineHeight = options?.lineHeight || textSize + 10
     let background = options?.background || '#ffffffaa'
@@ -25,13 +23,38 @@ export function createTextBaloon(manager, renderer, positions, text, options) {
     let anchor = options?.anchor || [0, 0]
     let paddingX = options?.paddingX || options?.padding || 10
     let paddingY = options?.paddingY || options?.padding || 10
+    let minWidth = options?.minWidth || 0
+    let minHeight = options?.minHeight || 0
+    let maxWidth = options?.maxWidth || 0
+    let textWidth = 0
+
+    let textChunks = chunkString(text)
     let shapes = textChunks.map((str) => {
         let shapeText = new ShapeText(textSize, str)
-        textWidth = Math.max(renderer.mesure(shapeText).width, textWidth)
-        return shapeText
+        let width = renderer.mesure(shapeText).width
+        let boxWidth = width + paddingX * 2
+        if (!maxWidth || boxWidth < maxWidth) {
+            textWidth = Math.max(width, textWidth)
+            return [shapeText]
+        }
+
+        for (let i = str.length - 1; 0 < i; i--) {
+            let strs = chunkStringLong(str, i)
+            let subShapes = strs.map((elStr) => new ShapeText(textSize, elStr))
+            let subShapesTextWidth = subShapes.map(elShape => renderer.mesure(elShape).width)
+            boxWidth = Math.max(...subShapesTextWidth.map(elTextW => elTextW + paddingX * 2))
+            if (boxWidth < maxWidth) {
+                textWidth = Math.max(textWidth, ...subShapesTextWidth)
+                return subShapes
+            }
+        }
+        return [shapeText]
     })
+    shapes = shapes.flat()
     let boxWidth = textWidth + paddingX * 2
+    boxWidth = Math.max(minWidth, boxWidth)
     let boxHeight = shapes.length * lineHeight + paddingY * 2
+    boxHeight = Math.max(minHeight, boxHeight)
     let posX = positions[0] - boxWidth * anchor[0]
     let posY = positions[1] - boxHeight * anchor[1]
     let boxes = createBodrededBoxColor(
