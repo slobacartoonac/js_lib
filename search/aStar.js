cf1bb452f7c5ad5daa6c282d8897e7a8f6ab2f93
Slobodan <slobacartoonac@gmail.com>
2023-07-03 10:46:13 +0200

    Remove unused

--- search/aStar.js
+++ search/aStar.js
@@ -3,7 +3,6 @@ class GridPoint {
     this.f = 0; //total cost function
     this.g = 0; //cost function from start to the current grid point
     this.h = 0; //heuristic estimated cost function from current grid point to the goal
-    this.neighbors = []; // neighbors of the current grid point
     this.parent = undefined; // immediate source of the current grid point
     this.position = position;
   }
