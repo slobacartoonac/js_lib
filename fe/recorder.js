3f65790dad50d7c3b498cc42474f6f7a73477845
Slobodan <slobacartoonac@gmail.com>
2023-10-20 10:42:22 +0200

    Add record then stop first canvas

---
+++ fe/recorder.js
@@ -0,0 +1,28 @@
+var recordedChunks = [];
+
+var time = 0;
+
+export const record3sec = (timeout) => new Promise(function (res, rej) {
+    var canvas = document.getElementsByTagName("canvas")[0];
+    var stream = canvas.captureStream(30);
+
+    let mediaRecorder = new MediaRecorder(stream, {
+        mimeType: "video/webm; codecs=vp9"
+    });
+
+    mediaRecorder.start(time);
+
+    mediaRecorder.ondataavailable = function (e) {
+        recordedChunks.push(e.data);
+    }
+
+    mediaRecorder.onstop = function (event) {
+        var blob = new Blob(recordedChunks, {
+            "type": "video/webm"
+        });
+        var url = URL.createObjectURL(blob);
+        res(url);
+    }
+    if(timeout)
+        setTimeout(()=>mediaRecorder.stop(), timeout)
+});
