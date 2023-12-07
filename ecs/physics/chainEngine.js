54302f119e6e2cf337be90aa5f21528cc00eca16
Slobodan <slobacartoonac@gmail.com>
2023-12-07 15:16:15 +0100

    Make chain force engine changable

--- ecs/physics/chainEngine.js
+++ ecs/physics/chainEngine.js
@@ -1,9 +1,14 @@
 import { Physics } from './physics.js'
 import { Transform } from './transform.js'
 
-function ChainLink(connects, distance) {
+const defaultForce = (distanceNormalised)=>{
+	return Math.atan((distanceNormalised - 1) * 5) + Math.pow(distanceNormalised - 1, 3)
+}
+
+function ChainLink(connects, distance, force = defaultForce) {
 	this.connects = connects
 	this.distance = distance
+	this.force = force
 }
 
 function computeChainForce(
@@ -11,7 +16,7 @@ function computeChainForce(
 	transform,
 ) {
 	var ret = [0, 0]
-	naibor.forEach(({ transform: naiborTranform, distance: naiborDistance }) => {
+	naibor.forEach(({ transform: naiborTranform, distance: naiborDistance, force }) => {
 		var distanceX = (naiborTranform.positions[0] - transform.positions[0])
 		var distanceY = (naiborTranform.positions[1] - transform.positions[1])
 		var distanceAngle = Math.atan2(distanceY, distanceX)
@@ -19,7 +24,7 @@ function computeChainForce(
 			distanceX * distanceX
 			+ distanceY * distanceY)
 		var distanceNormalised = distance / naiborDistance
-		var forceIntencity = (Math.atan((distanceNormalised - 1) * 5) + Math.pow(distanceNormalised - 1, 3))
+		var forceIntencity = force(distanceNormalised)
 		var forceComponents = {
 			x: Math.cos(distanceAngle) * forceIntencity,
 			y: Math.sin(distanceAngle) * forceIntencity
@@ -39,7 +44,7 @@ ChainEngine.prototype.compute = function () {
 	this.manager.getEnities(ChainLink).map(
 		(elem) => {
 			var chainLinks = this.manager.get(ChainLink, elem)
-			var naibor = chainLinks.map(link => ({ transform: this.manager.get(Transform, link.connects)[0], distance: link.distance }))
+			var naibor = chainLinks.map(link => ({ transform: this.manager.get(Transform, link.connects)[0], distance: link.distance, force: link.force }))
 			var transform = this.manager.get(Transform, elem)[0]
 			var physics = this.manager.get(Physics, elem)[0]
 			var force = computeChainForce(
