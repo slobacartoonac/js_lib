a679e0dee49a0d3c4688694e1c9f8cef56f052cb
Slobodan <slobacartoonac@gmail.com>
2024-09-25 16:02:02 +0200

    add chain damping

--- ecs/physics/chainEngine.js
+++ ecs/physics/chainEngine.js
@@ -1,7 +1,7 @@
 import { Physics } from './physics.js'
 import { Transform } from './transform.js'
 
-const defaultForce = (distanceNormalised)=>{
+const defaultForce = (distanceNormalised, distance, distanceGoal)=>{
 	return Math.atan((distanceNormalised - 1) * 5) + Math.pow(distanceNormalised - 1, 3)
 }
 
@@ -9,22 +9,47 @@ function ChainLink(connects, distance, force = defaultForce) {
 	this.connects = connects
 	this.distance = distance
 	this.force = force
+	this.dampening = 0.05
+}
+
+/**
+ * Description placeholder
+ *
+ * @param {number[]} force
+ * @param {number[]} velocity
+ * @returns {number[]}
+ */
+function dampenForceBasedOnVelocity(force, velocity, dampening) {
+	//project velocity on force
+	//make force smaller based on velocity
+	// use dot product
+	let velocityMagnitude = Math.sqrt(velocity[0] * velocity[0] + velocity[1] * velocity[1]);
+	let forceMagnitude = Math.sqrt(force[0] * force[0] + force[1] * force[1]);
+	let devider = velocityMagnitude * forceMagnitude
+	
+	if(devider == 0){
+		return force
+	}
+	let dotProduct = (force[0] * velocity[0] + force[1] * velocity[1]) / devider;
+	let dampingFactor = Math.max(0, 1 - dotProduct*dampening);
+	
+	return [force[0] * dampingFactor, force[1] * dampingFactor];
 }
 
 function computeChainForce(
-	naibor,
+	naibors,
 	transform,
 ) {
 	var ret = [0, 0]
-	naibor.forEach(({ transform: naiborTranform, distance: naiborDistance, force }) => {
+	naibors.forEach(({ transform: naiborTranform, distance: distanceGoal, force }) => {
 		var distanceX = (naiborTranform.positions[0] - transform.positions[0])
 		var distanceY = (naiborTranform.positions[1] - transform.positions[1])
 		var distanceAngle = Math.atan2(distanceY, distanceX)
 		var distance = Math.sqrt(
 			distanceX * distanceX
 			+ distanceY * distanceY)
-		var distanceNormalised = distance / naiborDistance
-		var forceIntencity = force(distanceNormalised)
+		var distanceNormalised = distance / distanceGoal
+		var forceIntencity = force(distanceNormalised, distance, distanceGoal)
 		var forceComponents = {
 			x: Math.cos(distanceAngle) * forceIntencity,
 			y: Math.sin(distanceAngle) * forceIntencity
@@ -44,13 +69,18 @@ ChainEngine.prototype.compute = function () {
 	this.manager.getEnities(ChainLink).map(
 		(elem) => {
 			var chainLinks = this.manager.get(ChainLink, elem)
-			var naibor = chainLinks.map(link => ({ transform: this.manager.get(Transform, link.connects)[0], distance: link.distance, force: link.force }))
+			var naibors = chainLinks.map(link => ({ transform: this.manager.get(Transform, link.connects)[0], distance: link.distance, force: link.force }))
 			var transform = this.manager.get(Transform, elem)[0]
-			var physics = this.manager.get(Physics, elem)[0]
+			var physics = this.manager.get(Physics, elem)[0] 
 			var force = computeChainForce(
-				naibor,
+				naibors,
 				transform
 			)
+
+			//dampen force based on velocity
+			
+			force = dampenForceBasedOnVelocity(force, physics.speeds, chainLinks[0].dampening)
+			
 			physics.applyForce(force)
 		}
 	)
